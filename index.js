464d1b5714d736ad91f2c15f4dd84d1e0418f91b
Brainstormed <aritramondal855@gmail.com>
2022-06-04 21:50:15 +0530

    Initiated the calculator project

---
+++ index.js
@@ -0,0 +1,25 @@
+let text = document.getElementById("text");
+text.textContent = 0;
+
+const del = document.getElementById("del");
+const c = document.getElementById("c");
+const ce = document.getElementById("ce");
+const percent = document.getElementById("percent");
+const divide = document.getElementById("divide");
+const sqrt = document.getElementById("sqrt");
+const squared = document.getElementById("squared");
+const half = document.getElementById("half");
+const mul = document.getElementById("mul");
+const nine = document.getElementById("nine");
+// const
+// const
+// const
+// const
+// const
+// const
+// const
+// const
+// const
+// const
+// const
+console.log(del);
